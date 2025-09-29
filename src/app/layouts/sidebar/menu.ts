1725ca2d0b1cd470d86b42a91cd39245acad6f25
Shubham Patel <ptlshubham@gmail.com>
2025-09-29 11:38:55 +0530

    all change

--- src/app/layouts/sidebar/menu.ts
+++ src/app/layouts/sidebar/menu.ts
@@ -37,34 +37,34 @@ export const MENU: MenuItem[] = [
             },
             {
                 id: 7,
-                label: 'Case-Studies',
+                label: 'Case Studies',
                 link: '/workfolio/casestudy',
                 parentId: 3
             },
         ]
     },
     {
-        id: 3,
-        label: 'placement',
-        icon: 'mdi mdi-briefcase-outline',
+        id: 8,
+        label: 'Placement',
+        icon: 'mdi mdi-account-search',
         subItems: [
             {
-                id: 4,
-                label: 'category',
+                id: 9,
+                label: 'Category',
                 link: '/placement/category',
-                parentId: 3
+                parentId: 8
             },
             {
-                id: 4,
-                label: 'sub-category',
+                id: 10,
+                label: 'Sub-Category',
                 link: '/placement/sub-category',
-                parentId: 3
+                parentId: 8
             },
             {
-                id: 4,
-                label: 'sub-to-subcategory',
+                id: 11,
+                label: 'Sub-to-Subcategory',
                 link: '/placement/sub-to-subcategory',
-                parentId: 3
+                parentId: 8
             },
 
         ]
